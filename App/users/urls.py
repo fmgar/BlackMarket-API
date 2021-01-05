95218f2f9d55a02e9445448a24f3b85690e8da36
Franklin Garcia <fmanuelgarcia@icloud.com>
2021-01-05 00:59:55 -0500

    Add email verification to user with jwt

--- App/users/urls.py
+++ App/users/urls.py
@@ -4,9 +4,14 @@
 from django.urls import path
 
 # Views
-from App.users.views import UserLoginAPIView, UserSignUpAPIView
+from App.users.views import (
+    UserLoginAPIView,
+    UserSignUpAPIView,
+    AccountVerificationAPIView
+)
 
 urlpatterns = [
     path('users/login/', UserLoginAPIView.as_view(), name='login'),
     path('users/signup/', UserSignUpAPIView.as_view(), name='signup'),
+    path('users/verify/', AccountVerificationAPIView.as_view(), name='verify'),
 ]
