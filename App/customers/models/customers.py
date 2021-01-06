7ea3967ae37bd13ef4b2ae142c87b1ecce0c7a86
Franklin Garcia <fmanuelgarcia@icloud.com>
2021-01-06 18:25:46 -0500

    I change email fields on Customer model I erased unique=True

--- App/customers/models/customers.py
+++ App/customers/models/customers.py
@@ -30,7 +30,6 @@ class Customer(BlackMarketModel):
 
     email = models.EmailField(
         'email address',
-        unique=True,
         error_messages={
             'unique': 'A user with that email already exists.'
         },
