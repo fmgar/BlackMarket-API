51bfeef4fe214b107be28f7b22df67ac9afb1751
Franklin Garcia <fmanuelgarcia@icloud.com>
2021-01-07 18:24:09 -0500

    Add user authentication

--- App/items/views/items.py
+++ App/items/views/items.py
@@ -2,6 +2,7 @@
 
 # Django REST framework
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Serializers
 from ..serializers.items import ItemModelSerializer
@@ -14,3 +15,11 @@ class ItemsViewSet(viewsets.ModelViewSet):
     """Item view set. """
     queryset = Item.objects.all()
     serializer_class = ItemModelSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Restrict list to is_active and is_superuser"""
+        queryset = Item.objects.all()
+        if self.action == 'list':
+            return queryset.filter(is_active=True)
+        return queryset
